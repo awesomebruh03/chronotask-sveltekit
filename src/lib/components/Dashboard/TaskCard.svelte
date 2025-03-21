4fdf10cf6037229211e19ecd4b9512cd9620c74e
Md Sazid Ullah <123637515+awesomebruh03@users.noreply.github.com>
2025-03-21 19:01:06 +0000

    Add Dashboard components: Chart, StatCard, and TaskCard with initial layout

---
+++ src/lib/components/Dashboard/TaskCard.svelte
@@ -0,0 +1,1 @@
+<h1> hi i am task card</h1>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Chart from '$lib/components/Dashboard/Chart.svelte';
+	import StatCard from '$lib/components/Dashboard/StatCard.svelte';
+	import TaskCard from '$lib/components/Dashboard/TaskCard.svelte';
+</script>
+
+<div class="mt-28">
+	<Chart />
+	<StatCard />
+	<TaskCard />
+</div>
